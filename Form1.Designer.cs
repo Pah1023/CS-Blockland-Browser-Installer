e6e53cbe42c2c1625919cf3d4d5dab7abf993859
My Name <my-name@chromium.org>
2021-12-01 12:51:03 -0500

    Initial Commit
    Initial Commit of project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace CSBlBrowserInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.linkLabel1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(989, 651);
+            this.splitContainer1.SplitterDistance = 491;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(492, 473);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Downloading file...";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(15, 463);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(471, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(960, 390);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(894, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Install";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Targeted Version:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(134, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(754, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(771, 463);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(204, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Blockland Browser installer V0.1 DevBuild";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Blockland Executable: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(894, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Choose...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(134, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(754, 20);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(979, 146);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(989, 651);
+            this.Controls.Add(this.splitContainer1);
+            this.MinimumSize = new System.Drawing.Size(580, 400);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Blockland Browser Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace CSBlBrowserInstaller
+{
+    public partial class Form1 : Form
+    {
+        public string latestXMLVersion;
+        int latestRelease = -1;
+        System.Xml.XmlDocument doc = null;
+        VersionComboItem[] ci = null;
+        public Form1()
+        {
+            InitializeComponent();
+            //Computer\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Valve\Steam InstallPath
+            //Blockland Game ID: 250340
+            //Need to check for file @ Steam\SteamApps\appmanifest_250340.acf
+            //If not found, check libraryfolders.vdf, and check all directories for said file.
+
+            // For now we'll just assume default location - the user can always set the location.
+            doPrint(linkLabel1.Text + "\r\n");
+            if(System.IO.File.Exists("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Blockland\\Blockland.exe"))
+            {
+                textBox2.Text = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Blockland";
+                doPrint("Found Blockland.exe in default Steam directory...\r\n");
+            } else
+            {
+                doPrint("Could not locate Blockland.exe, user needs to choose the location of it.\r\n");
+            }
+            queryLatestRelease();
+            client.DownloadProgressChanged += new System.Net.DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_processDownload);
+        }
+        class VersionComboItem
+        {
+            public int ID { get; set; }
+            public string Text { get; set; }
+            public System.Xml.XmlNode node { get; set; }
+        }
+        public void queryLatestRelease()
+        {
+            latestXMLVersion = (new System.Net.WebClient().DownloadString("https://www.pahs.site/blb/versions.xml"));
+            System.Xml.XmlReaderSettings set = new System.Xml.XmlReaderSettings();
+            set.ConformanceLevel = System.Xml.ConformanceLevel.Fragment;
+            System.Xml.XmlReader reader = System.Xml.XmlReader.Create(new System.IO.StringReader(latestXMLVersion), set);
+            doc = new System.Xml.XmlDocument();
+            
+
+            doc.Load(reader);
+            reader.Close();
+            int count = doc.GetElementsByTagName("release").Count;
+            
+            doPrint("Found " + count + " release(s).\r\n");
+            ci = new VersionComboItem[count];
+            int i = 0;
+            foreach (System.Xml.XmlNode node in doc.GetElementsByTagName("release"))
+            {
+                if (node.Attributes != null && node.Attributes["version"] != null)
+                {
+                    ci[i] = new VersionComboItem { ID = i, Text = "BL Browser Build #" + node.Attributes["version"].Value + " (Blockland " + node.SelectSingleNode(".//blocklandrelease").InnerText + ")", node = node};
+                    doPrint("Found Blockland Browser release #" + node.Attributes["version"].Value + "\r\n");
+                    Boolean isLatest = false;
+                    if(Boolean.TryParse(node.Attributes["latest"] != null ? node.Attributes["latest"].Value:"", out isLatest) && isLatest)
+                    {
+                        latestRelease = i;
+                    }
+                    i++;
+
+                }
+            }
+            comboBox1.DisplayMember = "Text";
+            comboBox1.ValueMember = "ID";
+            comboBox1.DataSource = ci;
+            if (latestRelease == -1)
+            {
+                doPrint("Was unable to find latest release of Blockland Browser.\r\n");
+            } else
+            {
+                selectRelease(latestRelease, true);
+            }
+        }
+        public void selectRelease(int x, bool changeItem)
+        {
+            if(changeItem)
+                comboBox1.SelectedItem = ci[x];
+            System.Data.DataTable table = new System.Data.DataTable();
+            table.Columns.Add("Install", typeof(bool)).ReadOnly = false;
+            table.Columns.Add("Dir.");
+            table.Columns.Add("File");
+            table.Columns.Add("Hash");
+            table.Columns.Add("Matches", typeof(bool)).ReadOnly = true;
+            
+            foreach (System.Xml.XmlNode node in ci[x].node.SelectNodes(".//file"))
+            {
+                System.Data.DataRow row = table.NewRow();
+                row["File"] = node.SelectSingleNode(".//name").InnerText;
+                row["Hash"] = node.SelectSingleNode(".//sha1").InnerText;
+                row["Dir."] = node.SelectSingleNode(".//directory").InnerText;
+                String dir = textBox2.Text + "\\" + node.SelectSingleNode(".//directory").InnerText + node.SelectSingleNode(".//name").InnerText;
+                String hash = getFileHash(dir);
+                //doPrint("Checking file " + dir + ", hash is " + hash + "\r\n");
+                row["Matches"] = hash.Equals(node.SelectSingleNode(".//sha1").InnerText);
+                row["Install"] = !(bool)row["Matches"];
+                table.Rows.Add(row);
+            }
+            dataGridView1.DataSource = table;
+            //dataGridView1.Rows[0].Cells[0].ReadOnly = true;
+            dataGridView1.Refresh();
+        }
+        public void checkBlocklandExecutable()
+        {
+            if(System.IO.File.Exists(textBox2.Text + "\\Blockland.exe"))
+            {
+                String hash = getFileHash(textBox2.Text + "\\Blockland.exe");
+                doPrint("\r\nNot fully implemented.\r\nBlockland.exe located, hash is " + hash + "\r\n");
+            }
+        }
+        public String getFileHash(String file)
+        {
+            if(System.IO.File.Exists(file))
+            {
+                using (System.IO.FileStream stream = System.IO.File.OpenRead(file))
+                {
+                    using (System.Security.Cryptography.SHA1Managed sha = new System.Security.Cryptography.SHA1Managed())
+                    {
+                        byte[] checksum = sha.ComputeHash(stream);
+                        return BitConverter.ToString(checksum).Replace("-", string.Empty);
+                    }
+                }
+            }
+            return "0";
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.pahs.site/blb/");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = textBox2.Text.Equals("") ? "c:\\" : (System.IO.Directory.Exists(textBox2.Text) ? textBox2.Text : "c:\\");
+            openFileDialog1.Filter = "Blockland.exe|Blockland.exe";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
+                selectRelease(comboBox1.SelectedIndex, false);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectRelease(comboBox1.SelectedIndex, false);
+        }
+        System.Collections.Generic.List<String> downloadQueue;
+        System.Net.WebClient client = new System.Net.WebClient();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Data.DataTable table = (System.Data.DataTable)dataGridView1.DataSource;
+            downloadQueue = new System.Collections.Generic.List<String>();
+            int i = 0;
+            int x = comboBox1.SelectedIndex;
+            foreach (System.Data.DataRow row in table.Rows)
+            {
+                if((Boolean)row["Install"])
+                {
+                    String url = "https://www.pahs.site/blb/" + ci[x].node.SelectNodes(".//file")[i].SelectSingleNode(".//serverdirectory").InnerText + ci[x].node.SelectNodes(".//file")[i].SelectSingleNode(".//name").InnerText;
+                    downloadQueue.Add(url + "\t" + row["File"] + "\t" + row["Dir."] + "\t" + i.ToString());
+                    doPrint("Attempting to download file " + url + " \r\n");
+                }
+                i++;
+            }
+            currIdx = -1;
+            client_doDownload();
+            
+        }
+        String currfile = "N/A";
+        int currIdx = -1;
+        private void client_doDownload()
+        {
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                doPrint("Current file count: " + downloadQueue.Count + "\r\n");
+
+                if (downloadQueue.Count <= 0)
+                {
+                    selectRelease(comboBox1.SelectedIndex, false);
+                    doPrint("Finished downloading all files");
+                    return;
+                }
+                String queue = downloadQueue[0];
+                String url = queue.Split('\t')[0];
+                String file = queue.Split('\t')[1];
+                String dir = queue.Split('\t')[2];
+                currIdx = int.Parse(queue.Split('\t')[3]);
+                doPrint("Downloading file " + dir + file + " from url " + url + "\r\n");
+                downloadQueue.RemoveAt(0);
+                if(!System.IO.Directory.Exists(textBox2.Text + "\\" + dir))
+                {
+                    System.IO.Directory.CreateDirectory(textBox2.Text + "\\" + dir);
+                }
+                if(file.Equals("Blockland.exe"))
+                {
+                    if(System.IO.File.Exists(textBox2.Text + "\\Blockland.exe"))
+                    {
+                        System.IO.FileAttributes att = System.IO.File.GetAttributes(textBox2.Text + "\\Blockland.exe");
+                        if(att.HasFlag(System.IO.FileAttributes.ReadOnly))
+                        {
+                            att = att & ~System.IO.FileAttributes.ReadOnly;
+                            System.IO.File.SetAttributes(textBox2.Text + "\\Blockland.exe", att);
+                        }
+                    }
+                }
+                currfile = file;
+
+                System.Threading.Thread thread = new System.Threading.Thread(() =>
+                {
+                        client.DownloadFileAsync(new Uri(url), textBox2.Text + "\\" + dir + file);
+                    
+                });
+                thread.Start();
+            });
+        }
+        private void client_processDownload(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                this.Invoke((MethodInvoker)delegate
+               {
+                   doPrint("Error downloading file " + currfile + "\r\n" + e.Error + "\r\n");
+               });
+            }
+            else
+            {
+                this.Invoke((MethodInvoker)delegate
+               {
+                   if(currfile.Equals("Blockland.exe"))
+                   {
+                       System.IO.FileAttributes att = System.IO.File.GetAttributes(textBox2.Text + "\\Blockland.exe");
+                       att = att | System.IO.FileAttributes.ReadOnly;
+                       System.IO.File.SetAttributes(textBox2.Text + "\\Blockland.exe", att);
+
+                   }
+                   System.Data.DataTable table = (System.Data.DataTable)dataGridView1.DataSource;
+                   table.Rows[currIdx]["Install"] = false;
+               });
+                client_doDownload();
+            }
+        }
+        private void client_DownloadProgressChanged(object sender, System.Net.DownloadProgressChangedEventArgs args)
+        {
+            this.BeginInvoke((MethodInvoker)delegate
+           {
+               double bytesIn = double.Parse(args.BytesReceived.ToString());
+               double total = double.Parse(args.TotalBytesToReceive.ToString());
+               double per = bytesIn / total * 100;
+               
+               progressBar1.Value = int.Parse(Math.Truncate(per).ToString());
+               label3.Text = "Downloading file " + currfile + "... " + progressBar1.Value.ToString() + "%";
+           });
+        }
+        System.IO.FileStream log = System.IO.File.Open("output.log", System.IO.FileMode.Create);
+        private void doPrint(String txt)
+        {
+
+            textBox1.AppendText(txt);
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(txt);
+            log.Write(bytes, 0, bytes.Length);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
